a8bd67c36a58a75f646d92df44b8cc50ac45ba0b
Ahmed Wez <ahmed.moh.wez@gmail.com>
2026-02-21 16:11:19 +0200

    Create __init__.py

---
+++ realm/modules/__init__.py
@@ -0,0 +1,14 @@
+"""
+Modular Components for REALM-CL
+
+Includes:
+- Module: Individual functional module
+- ModularNetwork: Dynamic module composition system
+"""
+
+from realm.modules.modular_network import Module, ModularNetwork
+
+__all__ = [
+    "Module",
+    "ModularNetwork",
+]
