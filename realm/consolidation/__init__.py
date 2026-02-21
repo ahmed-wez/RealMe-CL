948619a8ee4843d182238f441cfef00e25c8d753
Ahmed Wez <ahmed.moh.wez@gmail.com>
2026-02-21 16:11:01 +0200

    Create __init__.py

---
+++ realm/consolidation/__init__.py
@@ -0,0 +1,12 @@
+"""
+Consolidation Mechanisms for REALM-CL
+
+Includes:
+- SleepConsolidation: Offline sleep-like replay and consolidation
+"""
+
+from realm.consolidation.sleep_consolidation import SleepConsolidation
+
+__all__ = [
+    "SleepConsolidation",
+]
